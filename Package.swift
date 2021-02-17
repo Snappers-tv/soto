fa1906b97daf40aa08fd6af4b64ded701f67afae
Alon Genosar <alon@bootleg.co.il>
2021-02-17 08:21:33 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ import PackageDescription
 
 let package = Package(
     name: "soto",
-    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    platforms: [.iOS(.v13), .tvOS(.v12), .watchOS(.v5)],
     products: [
         .library(name: "SotoACM", targets: ["SotoACM"]),
         .library(name: "SotoACMPCA", targets: ["SotoACMPCA"]),
